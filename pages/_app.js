f93118be3542264b6514f16e687474622b636de0
Slavi15 <slavirezashkiofficial@gmail.com>
2020-12-30 21:18:03 +0200

    Add dropdowns and dropzone prototype

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,14 @@ import '../styles/Convert.module.scss';
 import Layout from '../components/Layout.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 import { createGlobalStyle } from "styled-components";
 import { config, dom } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 const GlobalStyles = createGlobalStyle`${dom.css()}`;
 
-library.add(fab, faBars, faTimes);
+library.add(fab, faBars, faTimes, faArrowRight);
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/convert/index.js
+++ pages/convert/index.js
@@ -1,9 +1,52 @@
 import styles from '../../styles/Convert.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useDropzone } from 'react-dropzone';
 
 const Convert = () => {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+    const files = acceptedFiles.map(file => (
+        <table>
+            <tr>
+                <th>File</th>
+                <th>Size</th>
+            </tr>
+            <tr>
+                <td>{file.path}</td>
+                <td>{file.size}</td>
+            </tr>
+        </table>
+    ));
     return (
         <div>
             <div className={styles.convslogan}>Create, Select, Convert!</div>
+            <div className={styles.select}>
+                <select name="files1" className={styles.selectcontent}>
+                    <option value="select">Select</option>
+                    <option value="txt">txt</option>
+                    <option value="pdf">pdf</option>
+                    <option value="jpg">jpg</option>
+                    <option value="png">png</option>
+                </select>
+                <FontAwesomeIcon className={styles.arrow} icon="arrow-right" />
+                <select name="files2" className={styles.selectcontent}>
+                    <option value="select">Select</option>
+                    <option value="txt">txt</option>
+                    <option value="pdf">pdf</option>
+                    <option value="jpg">jpg</option>
+                    <option value="png">png</option>
+                </select>
+            </div>
+            <section className={styles.container}>
+                <div className={styles.innercontainer} {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <div className={styles.textcontent}>Drag 'n' drop some files!</div>
+                </div>
+                <aside>
+                    <div className={styles.filestext}>Files</div>
+                    <div>{files}</div>
+                </aside>
+            </section>
         </div>
     )
 }
